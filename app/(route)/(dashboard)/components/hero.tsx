07839324f88c210725b369542c9c26c3eb18d1af
Sherwin2030 <sherwinmontoya2026@gmail.com>
2025-08-11 14:12:01 +0800

    Updated here.tsx
    update margin and padding text columns

--- app/(route)/(dashboard)/components/hero.tsx
+++ app/(route)/(dashboard)/components/hero.tsx
@@ -410,7 +410,7 @@ const HeroSection = () => {
                     <div class="w-12 h-12 bg-gray-50 rounded-lg flex items-center justify-center shadow-sm group-hover:shadow-md transition-shadow float-left">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-building w-6 h-6 text-orange-500"><rect width="16" height="20" x="4" y="2" rx="2" ry="2"></rect><path d="M9 22v-4h6v4"></path><path d="M8 6h.01"></path><path d="M16 6h.01"></path><path d="M12 6h.01"></path><path d="M12 10h.01"></path><path d="M12 14h.01"></path><path d="M16 10h.01"></path><path d="M16 14h.01"></path><path d="M8 10h.01"></path><path d="M8 14h.01"></path></svg>
                     </div>
-                    <h3 class="text-sm font-bold text-gray-800 float-left p-3">
+                    <h3 class="text-sm font-bold text-gray-800 float-left p-4 ms-1">
                         Buy
                     </h3>
                   </a>
@@ -422,7 +422,7 @@ const HeroSection = () => {
                     <div class="w-12 h-12 bg-gray-50 rounded-lg flex items-center justify-center shadow-sm group-hover:shadow-md transition-shadow float-left">
                       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trending-up w-6 h-6 text-orange-500"><polyline points="22 7 13.5 15.5 8.5 10.5 2 17"></polyline><polyline points="16 7 22 7 22 13"></polyline></svg>
                     </div>
-                    <h5 class="text-sm font-bold text-gray-800 float-left p-3">
+                    <h5 class="text-sm font-bold text-gray-800 float-left p-4 ms-1">
                       Pre-Sale
                     </h5>
                   </a>
